ec3b4dd588af17c227ca51ae9fba454cf83e3d68
Mikhail Rozbitskiy <mikhailrozbitskiy@MacBook-Pro-Mikhail.local>
2019-04-08 16:51:39 +0300

    API connect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,54 @@
 import React, { Component } from 'react';
-import './App.css';
 
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
+
+  componentDidMount() {
+
+    fetch('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json,
+        })
+      });
+
+  }
+
+  render() {
+
+    var { isLoaded, items } =this.state;
+
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    }
+
+    else {
+
+      return (
+        <div className="App">
+
+          <ul>
+            {items.map(item => (
+              <li key={item.ccy}>
+                Currency {item.ccy} | Value {item.rate}
+              </li>
+            ))}
+          </ul>
+
+        </div>
+      );
+
+    }
+  }
+}
 
 export default App;
